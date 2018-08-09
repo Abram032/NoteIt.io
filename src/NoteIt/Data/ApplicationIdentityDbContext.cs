373c7af9553ce179c02063d5b5541d31d2a0b8aa
Maissae <adamix224@gmail.com>
2018-08-09 02:15:50 +0200

    Added project files

---
+++ src/NoteIt/Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace NoteIt.Data
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(_configuration["ASPNETCORE_ENVIRONMENT"].Equals("Development"))
+                optionsBuilder.UseInMemoryDatabase("DevDb");
+            else
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
